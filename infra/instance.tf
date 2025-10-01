76a565aa198b879a663a23dab916ea22e516f392
ramkis7 <169043618+ramkis7@users.noreply.github.com>
2025-10-01 18:32:55 +0530

    Update instance.tf

--- infra/instance.tf
+++ infra/instance.tf
@@ -1,10 +1,9 @@
 resource "aws_instance" "capstone" {
-  ami           = data.aws_ami.amazon_linux.id
+  ami           = "ami-0c2b8ca1dad447f8a"  # Amazon Linux 2, us-east-1
   instance_type = "t2.medium"
   key_name      = aws_key_pair.deployer.key_name
   vpc_security_group_ids = [aws_security_group.capstone_sg.id]
-
-  user_data = file("${path.module}/bootstrap.sh")
+  user_data     = file("${path.module}/bootstrap.sh")
 
   tags = {
     Name = "capstone-devops"
